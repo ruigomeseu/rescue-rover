615adb704d6f569c413bc78b5d1afd9f5aba9251
Rui Gomes <ruigomeseu@gmail.com>
2014-05-29 14:40:07 +0100

    stationary robot initialized

--- src/rescuerover/logic/StationaryRobot.java
+++ src/rescuerover/logic/StationaryRobot.java
@@ -2,7 +2,14 @@ package rescuerover.logic;
 
 public class StationaryRobot extends Robot {
 
+    private int framesPerMovement;
+    private int framesLeft;
+    private boolean alive;
+
     public StationaryRobot(int x, int y, int direction) {
         super(x, y, direction);
+        sprite = new Sprite(9, 5, 32, "/robot.png");
     }
+
+
 }
